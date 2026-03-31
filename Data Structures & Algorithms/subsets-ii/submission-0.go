88049bb8ebbeb7fde8af65336959925561f63037
Cosma Tudor-Mihai <111079498+cosmatudor@users.noreply.github.com>
2026-03-31 15:40:15 +0300

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.go
@@ -0,0 +1,40 @@
+func subsetsWithDup(nums []int) [][]int {
+	res := [][]int{[]int{}}
+	sol := []int{}
+
+	sort.Ints(nums)
+
+	var dfs func(i int) 
+	dfs = func(i int) {
+		if i >= len(nums) {
+			return
+		}
+
+		sol = append(sol, nums[i])
+
+		if len(sol) > 0 && len(sol) <= len(nums) {
+			temp := make([]int, len(sol))
+			copy(temp, sol)
+			res = append(res, temp)
+		}
+		
+		dfs(i+1)
+		sol = sol[:len(sol)-1]
+		
+		for i + 1 < len(nums) && nums[i] == nums[i+1] {
+			i++
+		}
+		dfs(i+1)
+	}
+
+	dfs(0)
+	return res
+}
+
+// 1 1 2
+
+// 1
+// 1 1
+// 1 1 2
+// 1 2
+// 2
