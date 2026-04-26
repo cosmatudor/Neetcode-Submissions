ff4cc0e71468ae15874d6a4ce567dabd09dced8e
Cosma Tudor-Mihai <111079498+cosmatudor@users.noreply.github.com>
2026-04-26 17:37:54 +0300

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.go
@@ -0,0 +1,32 @@
+func generateParenthesis(n int) []string {
+	res := []string{}
+	currSol := ""
+
+	var dfs func(int, int) 
+	dfs = func(open, closed int) {
+		if open == closed && open == n {
+			res = append(res, currSol)
+			return
+		}
+
+		if closed > open {
+			return 
+		}
+
+		if open < n {
+			currSol += "("
+			dfs(open+1, closed)
+			currSol = currSol[:len(currSol)-1]
+		}
+
+		if closed < open {
+			currSol += ")"
+			dfs(open, closed+1)
+			currSol = currSol[:len(currSol)-1]
+		}
+	}
+
+	dfs(0, 0)
+
+	return res
+}
