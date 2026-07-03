86cbbffcf5aefb28f5432b1bff9c092e9979a765
Cosma Tudor-Mihai <111079498+cosmatudor@users.noreply.github.com>
2026-07-03 19:33:02 +0300

    Add: count-good-nodes-in-binary-tree - submission-2

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-2.go
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func goodNodes(root *TreeNode) int {
+    res := 0
+
+	var dfs func(prev, curr *TreeNode)
+	dfs = func(prev, curr *TreeNode) {
+		if curr == nil {
+			return
+		}
+
+			if prev.Val > curr.Val {
+				curr.Val = prev.Val
+			} else {
+				res++
+			}
+
+		dfs(curr, curr.Left)
+		dfs(curr, curr.Right)
+	}
+
+	prev := TreeNode{
+		Val: -101,
+	}
+	dfs(&prev, root)
+
+	return res
+}
